543b88ec0e5987c073ab7892c8507d5b9d649ed5
bree256 <141923968+bree256@users.noreply.github.com>
2024-03-27 12:35:59 +0300

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "../src/Components/Login/Login/index";
+import ErrorLogin from "../src/Components/ErrorLogin/index";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/error",
+    element: <ErrorLogin />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="Login">
+    //   <Login />
+    // </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
