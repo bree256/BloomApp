0472c5159332587b3a4a63deae629f49d353d74a
bree256 <141923968+bree256@users.noreply.github.com>
2024-04-29 18:34:12 +0300

    added netlify file

--- src/Components/HairServices/HairBusiness.jsx
+++ src/Components/HairServices/HairBusiness.jsx
@@ -1,17 +1,14 @@
-import { HairServicesHeader } from "./HairServices";
+// import { HairServicesHeader } from "./HairServices";
 
-
-export const HairBusiness = () =>{
-    return(
-        <div>
-            <HairServicesHeader/>
-            <div>
-               <h2>
-                Triple D Salon
-                </h2> 
-                <p>Plot 74 Ademola Adetokumbo way, Victoria Island, Lagos State</p>
-                <p>...making you standout beautifully!</p>
-            </div>
-        </div>
-    )
-}
+export const HairBusiness = () => {
+  return (
+    <div>
+      <HairServicesHeader />
+      <div>
+        <h2>Triple D Salon</h2>
+        <p>Plot 74 Ademola Adetokumbo way, Victoria Island, Lagos State</p>
+        <p>...making you standout beautifully!</p>
+      </div>
+    </div>
+  );
+};
